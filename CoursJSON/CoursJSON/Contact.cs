a4849c077377f3831f56b8dcc65bce674415b6c1
Benjamin DAGUE <bdague@grindboxlab.com>
2024-09-18 10:14:55 +0200

    Ajout d'une présentation de la sérialization / désérialization

---
+++ CoursJSON/CoursJSON/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursJSON;
+internal class Contact
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string? NickName { get; set; }
+}
---
+++ CoursJSON/CoursJSON/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+
+namespace CoursJSON;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        List<Contact> contacts =
+        [
+            new()
+            {
+                FirstName = "Benjamin",
+                LastName = "DAGUE",
+            },
+            new()
+            {
+                FirstName = "Jean",
+                LastName = "DUPONT",
+            }
+        ];
+
+        string serializedContact = JsonSerializer.Serialize(contacts);
+
+        Console.WriteLine(serializedContact);
+
+        serializedContact = serializedContact.Replace("Benjamin", "Jean");
+
+        Console.WriteLine(serializedContact);
+
+        //string filePath = "C:\\_workdir\\contact.json";
+        string filePath = @"C:\_workdir\contact.json";
+
+        File.WriteAllText(filePath, serializedContact);
+
+        string serializedContactFromFile = File.ReadAllText(filePath);
+
+        Console.WriteLine(serializedContactFromFile);
+
+        contacts = JsonSerializer.Deserialize<List<Contact>>(serializedContactFromFile) 
+            ?? throw new Exception("Le fichier ne contient pas un contact valide");
+
+        Console.ReadKey();
+    }
+}
