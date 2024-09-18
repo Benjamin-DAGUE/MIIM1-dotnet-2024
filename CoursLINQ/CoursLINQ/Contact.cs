d9504ae0ac43a041cc00158becd66675c4340dcb
Benjamin DAGUE <bdague@grindboxlab.com>
2024-09-18 14:39:34 +0200

    Ajout du cours LINQ

---
+++ CoursLINQ/CoursLINQ/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursLINQ;
+internal class Contact
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required DateTime BirthDate { get; set; }
+}
