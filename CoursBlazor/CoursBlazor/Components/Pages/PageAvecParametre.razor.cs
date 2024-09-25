55a3e8114c7a5ff9f94dac0aa6d7786dd568d774
Benjamin DAGUE <bdague@grindboxlab.com>
2024-09-25 14:17:40 +0200

    Mise en place d'un binding

--- CoursBlazor/CoursBlazor/Components/Pages/PageAvecParametre.razor.cs
+++ CoursBlazor/CoursBlazor/Components/Pages/PageAvecParametre.razor.cs
@@ -8,10 +8,14 @@ public partial class PageAvecParametre
     [Parameter]
     public int Id { get; set; }
 
+    [Parameter]
+    public EventCallback<int> IdChanged { get; set; }
+
     private void IncrementCount()
     {
         //Ceci est exécuté côté serveur
         Id++;
+        IdChanged.InvokeAsync(Id);
         if (NavigationManager.ToBaseRelativePath(NavigationManager.Uri).StartsWith("page/"))
         {
             NavigationManager.NavigateTo("page/" + Id);
